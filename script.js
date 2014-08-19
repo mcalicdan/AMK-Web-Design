e175a3d349854e1638c08ab1a201c701b62542ee
cuppajiu <lilyjgeorge@gmail.com>
2014-08-19 15:25:26 -0700

    make JS into jQuery

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-function checkname() {                                          // Declare function
-  var elMsg = document.getElementById("feedback");              // Get feedback element
-  if (this.value.length < 2) {                                  // If name too short
-    elMsg.textContent = "Name must be 2 characters or more";    // Set msg
-  } else {                                                      // Otherwise
-    elMsg.textContent = "Your name has been added to our records.";                                     // Clear msg
-  }
-}
-
-var elname = document.getElementById("name");                   // Get name input
-// When it loses focus call checkname()
-elname.addEventListener("blur", checkname, false);
+$(document).ready(function() {
+  $("#name").keyup(function() {
+    var feedback = $("#feedback");
+      if (this.value.length < 2) {
+        feedback.html("<p id='feedbackStuff'>Name must be 2 characters or more.</p>");
+      } else {
+        feedback.html("<p id='feedbackStuff'>Your name has been added to our records.</p>");
+      };
+  });
+});
 
 function checkemail(){
   var email = document.getElementById("emailfeedback");
