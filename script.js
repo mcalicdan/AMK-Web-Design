2da2a116513e8fcc45ce6ba6956bbdaac3c237e7
mcalicdan <miriamjoycalicdan@gmail.com>
2014-08-19 16:34:53 -0700

    changes to email portion

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-function checkname() {                                          // Declare function
-  var elMsg = document.getElementById("feedback");              // Get feedback element
-  if (this.value.length < 2) {                                  // If name too short
-    elMsg.textContent = "Name must be 2 characters or more";    // Set msg
-  } else {                                                      // Otherwise
-    elMsg.textContent = "Your name has been added to our records.";                                     // Clear msg
-  }
-}
+$(document).ready(function() {
+  $("#name").keyup(function() {
+    var feedback = $("#feedback");
+      if (this.value.length < 2) {
+        feedback.html("<p id='feedbackStuff'>Name must be 2 characters or more.</p>");
+      } else {
+        feedback.html("<p id='feedbackStuff'>Your name has been added to our records.</p>");
+      };
+  });
+});
+$(document).ready(function() {
+  $("#email").keyup(function() {
+    var emailfeedback = $("#emailfeedback");
+      if (this.value.length < 2) {
+        emailfeedback.html("<p id='feedback'>Your e-mail must be 2 characters or more.</p>");
+      } else {
+        emailfeedback.html("<p id='feedback'>Your e-mail has been added to our records.</p>");
+      };
+  });
+});
 
-var elname = document.getElementById("name");                   // Get name input
-// When it loses focus call checkname()
-elname.addEventListener("blur", checkname, false);
 
-function checkemail(){
-  var email = document.getElementById("emailfeedback");
-  if (this.value.length < 2) {
-    email.textContent = "You must submit a valid e-mail address";
-  }
-  else{
-    email.textContent = "Your e-mail has been added to our records";
-  }
-}
 
-var elemail = document.getElementById("email");
-elemail.addEventListener("blur", checkemail, false);
